ec1853ee02456eb9cd5aee4c22d2c4a5264f2e3a
blueLibertas <48977865+blueLibertas@users.noreply.github.com>
2026-01-25 07:34:20 +0000

    Deploying to gh-pages from @ blueLibertas/bluelibertas.github.io@63b167a33aecd6a7cfd55514e9afc6c87a06b427 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "All of my projects",
+          description: "All of my fun projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
